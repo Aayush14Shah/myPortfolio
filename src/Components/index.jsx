6d6d7e70b057ab3286799ec0d43f46e8cf431550
Aayush shah <132427969+Aayush14Shah@users.noreply.github.com>
2024-09-15 17:26:32 +0530

    Changes in about page

--- src/Components/index.jsx
+++ src/Components/index.jsx
@@ -43,7 +43,7 @@ const Index = (props) => {
         <div
           ref={container}
           className={` bg-[url('/public/images/indexBg.avif')] bg-cover bg-center rounded-md h-[500px] lg:h-[500px] md:h-[500px] max-w-[calc(100%-10%)] min-w-[1000px] lg:min-w-[700px] md:max-w-[800px] md:min-w-[500px]
-          sm:max-w-[calc(100%-6%)] sm:min-w-full sm:h-full p-[2%] flex flex-row  ${
+          sm:max-w-[calc(100%-6%)] sm:min-w-full sm:h-full p-[2%]   ${
             data.mode ? "bg-indigo-100" : "bg-[#313552]"
           }  ${
             data.mode ? "text-slate-800" : "text-white"
@@ -52,7 +52,7 @@ const Index = (props) => {
           {/* Short description */}
           <div
             ref={box1}
-            className={` p-3 lg:p-8 md:p-5 sm:p-2 rounded-md leading-8`}
+            className={`p-3 lg:p-2 md:p-5 sm:p-2 rounded-md leading-8`}
           >
             <div>
               <p
@@ -64,7 +64,7 @@ const Index = (props) => {
                 className={`text-left text-5xl lg:text-4xl md:text-4xl sm:text-3xl font-semibold tracking-8 my-2 text-indigo-800 `}
               >
                 Aayush Shah.
-                <p className={`mt-4`}>I build things for the web</p>
+                <p className={`mt-4 break-words `}>I build things for the web</p>
               </p>
               <span
                 className={`text-xl sm:text-lg text-stone-800 `}
@@ -87,7 +87,7 @@ const Index = (props) => {
                 id="talk"
                 type="button"
                 onClick={() => data.scrollHandler(data.contact)}
-                className={` text-[16px] text-stone-800 transition transform ease-linear duration-150 hover:scale-110 px-[20px] sm:px-[6px] absolute bottom-10 sm:my-3 py-1 rounded-lg font-semibold border-2 border-indigo-800 hover:text-white hover:bg-violet-800 `}
+                className={` text-[16px] text-stone-800 transition transform ease-linear duration-150 hover:scale-110 px-[20px] sm:px-[6px] absolute bottom-10 sm:bottom-[25%] sm:my-3 py-1 rounded-lg font-semibold border-2 border-indigo-800 hover:text-white hover:bg-violet-800 `}
               >
                 Let's talk <ArrowOutwardIcon />
               </button>
@@ -102,9 +102,9 @@ const Index = (props) => {
           </div>
           {/* Profile Photo */}
 
-          <div className="relative Image rounded-full md:mx-[10px] my-auto md:absolute md:bottom-5 md:right-2 sm:absolute sm:bottom-6 sm:right-8 ">
+          <div className=" Image rounded-full md:mx-[10px] my-auto absolute bottom-[3%] right-[5%] ">
             <img
-              className={` shadow-indigo-600/80 shadow-2xl w-[300px] lg:max-w-[250px] lg:min-w-[200px] md:max-w-[200px] sm:max-w-[130px] h-[300px] lg:max-h-[250px] lg:min-h-[200px] md:max-h-[200px] sm:max-h-[130px] object-cover rounded-full `}
+              className={` shadow-indigo-600/80 shadow-2xl w-[250px] lg:max-w-[230px] lg:min-w-[200px] md:max-w-[200px] sm:max-w-[150px] h-[450px] lg:max-h-[430px] lg:min-h-[400px] md:max-h-[200px] sm:max-h-[150px] object-cover rounded-full my-auto `}
               src={`${process.env.PUBLIC_URL}/Images/profile.jpg`}
               alt="My profile"
             />
--- src/Components/section2.jsx
+++ src/Components/section2.jsx
@@ -6,10 +6,39 @@ import SignalCellularAltIcon from "@mui/icons-material/SignalCellularAlt";
 // import AboutPopUp from "./aboutPopUp";
 // import gsap from "gsap";
 
-
 const AboutPopUp = lazy(() => import("./aboutPopUp"));
 
 const Section2 = (props) => {
+  const defaultRangeColor = "#313552";
+  const [bars, setBars] = useState([
+    { value: 95, color: defaultRangeColor, tag: "HTML" },
+    { value: 85, color: defaultRangeColor, tag: "CSS" },
+    { value: 60, color: defaultRangeColor, tag: "Javascript" },
+    { value: 60, color: defaultRangeColor, tag: "React" },
+  ]);
+
+  useEffect(() => {
+    const animateBars = setTimeout(() => {
+      setBars((prevBars) =>
+        prevBars.map((bar) => ({
+          ...bar,
+          value: Math.min(bar.value + 1, bar.finalValue), // For animated progress
+        }))
+      );
+    }, 10);
+    return () => clearTimeout(animateBars);
+  }, [bars]);
+
+  // Define the final percentages if you want animation
+  useEffect(() => {
+    setBars([
+      { value: 0, finalValue: 95, color: defaultRangeColor, tag: "HTML" },
+      { value: 0, finalValue: 85, color: defaultRangeColor, tag: "CSS" },
+      { value: 0, finalValue: 60, color: defaultRangeColor, tag: "Javascript" },
+      { value: 0, finalValue: 60, color: defaultRangeColor, tag: "React" },
+    ]);
+  }, []);
+
   const data = useContext(AppContext);
 
   const xlMinScreenWidth = 1080;
@@ -130,48 +159,44 @@ const Section2 = (props) => {
       id: 4,
       title: "Designing",
     },
-    {
-      id: 5,
-      title: "Development",
-    },
   ];
 
   return (
     <div
       ref={props.reference}
-      className={`w-full h-screen p-5 lg:p-2 md:p-2 grid grid-rows-3 grid-flow-col lg:grid-flow-row md:grid-flow-row sm:grid-flow-row gap-4 sm:space-y-4 justify-around  ${
+      className={`w-full h-screen p-5 lg:p-2 md:p-2 grid grid-rows-3 grid-flow-col lg:grid-flow-row md:grid-flow-row sm:grid-flow-row gap-[20px] sm:space-y-4 justify-around  ${
         data.mode ? "bg-gray-100" : "bg-[#1f2235]"
       } `}
     >
       {/* {mobileScreen ? ( */}
-      <div className="sm:w-full h-auto mx-auto my-auto row-span-1 col-span-2 sm:block hidden  ">
+      <div className="sm:w-full h-auto mx-auto my-auto row-span-1 col-span-2 sm:block hidden ">
         {mobileScreen && (
           <Suspense fallback="Loading...">
             <AboutPopUp btnTitle="About Me" hobbies={hobbies} />
           </Suspense>
-        )} 
+        )}
       </div>
       {/* ) : ( */}
       <div
-        className={`sm:hidden block text-wrap text-justify rounded-md w-full xl:w-full lg:w-full md:w-full sm:w-full h-full p-5 lg:p-5 md:p-[15px] sm:p-3 md:overflow-y-auto sm:overflow-y-auto my-auto row-span-3 lg:row-span-2 md:row-span-2
+        className={`sm:hidden block rounded-md w-full xl:w-full lg:w-full md:w-full sm:w-full h-full p-5 lg:p-5 md:p-[15px] sm:p-3 md:overflow-y-auto sm:overflow-y-auto my-auto row-span-3 lg:row-span-2 md:row-span-2 
         sm:row-span-1 sm:col-span-2 mx-auto lg:mx-[5px] md:mx-[0px] sm:mx-[0px] ${
           data.mode ? "bg-indigo-100" : "bg-[#313552]"
         } ${data.mode ? "text-slate-800" : "text-white"} lg:overflow-y-scroll `}
       >
         <h1
           id="text"
-          className=" text-3xl md:text-2xl sm:text-2xl font-semibold font-stone-400   "
+          className=" text-3xl md:text-2xl sm:text-2xl font-semibold font-stone-400 text-indigo-900  "
         >
           {/* bg-gradient-to-r from-pink-500 to-violet-500 bg-clip-text text-transparent */}
           About me
         </h1>
         <div className="my-[2%] flex md:flex-col justify-between  ">
           <img
-            className=" block w-[250px] h-[350px] xl:w-[200px] xl:h-[300px] lg:w-[180px] lg:h-[265px] md:w-[180px] md:h-[250px] rounded-lg "
+            className=" block w-[250px] h-[350px] xl:w-[200px] xl:h-[300px] lg:w-[180px] lg:h-[265px] md:w-[180px] md:h-[250px] rounded-md "
             src={`${process.env.PUBLIC_URL}/Images/profile2.jpg`}
             alt="My profile"
           />
-          <div className="w-[300px] lg:w-full xl:w-full md:w-full mx-[3%] md:mx-[1%] lg:space-y-1 xl:space-y-1.5 space-y-4  ">
+          <div className="w-[calc(100%-5%)] lg:w-full xl:w-full md:w-full mx-[3%] md:mx-[1%] space-y-2  ">
             <h1
               className={`text-xl font-semibold text-black ${
                 data.mode ? "text-slate-800" : "text-white"
@@ -183,7 +208,7 @@ const Section2 = (props) => {
               <div
                 id="hobby"
                 key={item.id}
-                className={`text-lg lg:text-sm font-semibold p-2 border-dotted flex justify-between ${
+                className={`text-lg lg:text-sm font-semibold p-2 border flex justify-between  ${
                   data.mode
                     ? "text-violet-800 border-2 border-violet-600"
                     : "text-yellow-500 border-2 border-amber-500"
@@ -195,7 +220,7 @@ const Section2 = (props) => {
             ))}
           </div>
         </div>
-        <p className="my-2 text-lg sm:text-sm font-blue-500 leading-8 xl:leading-normal sm:leading-2 xl:tracking-normal tracking-wide sm:tracking-medium text-justify sm:line-clamp-2 md:line-clamp-8">
+        <p className="my-2 text-[16px] sm:text-sm font-blue-500 leading-8 xl:leading-normal sm:leading-2 xl:tracking-normal tracking-wide sm:tracking-medium text-justify sm:line-clamp-2 md:line-clamp-8">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est
           doloremque aperiam nesciunt accusantium sed officiis deserunt dolores
           repellendus sint qui. Porro provident quaerat vero expedita eveniet
@@ -206,32 +231,63 @@ const Section2 = (props) => {
       </div>
       {/* )} */}
       <div
-        className={`rounded-md text-justify w-[800px] xl:w-[520px] lg:w-[320px] md:w-[300px] sm:w-[calc(100%-10px)] h-full  p-5 lg:p-5 md:p-[15px] sm:p-3 md:overflow-y-auto sm:overflow-y-auto my-auto col-span-2 lg:col-span-1 md:col-span-1 sm:row-span-1 sm:col-span-2 row-span-2 sm:row-span-2 lg:row-span-2 md:row-span-2 mx-auto lg:mx-[5px] -sm:mx-auto ${
+        className={`rounded-md text-justify w-[800px] xl:w-[520px] lg:w-[320px] md:w-[300px] sm:w-[calc(100%-10px)] h-full  p-5 lg:p-5 md:p-[15px] sm:p-3 md:overflow-y-auto sm:overflow-y-auto my-auto col-span-2 lg:col-span-1 md:col-span-1 sm:col-span-2 row-span-3 sm:row-span-2 lg:row-span-2 md:row-span-2 mx-auto lg:mx-[5px] -sm:mx-auto ${
           data.mode ? "bg-indigo-100" : "bg-[#313552]"
-        } ${data.mode ? "text-slate-800" : "text-white"}  `}
+        } ${data.mode ? "text-slate-800" : "text-white"} flex gap-[20px] `}
       >
-        <h1 className=" text-3xl md:text-2xl sm:text-2xl font-semibold font-stone-400 w-auto">
-         Why would you hire me?
-        </h1>
-        {skills.map((item) => (
-          <div
-            key={item.id}
-            className={`my-4 lg:my-2.5 md:my-2.5 text-lg sm:text-sm text-justify  flex flex-col`}
-          >
+        <div className="w-full ">
+          <h1 className="text-indigo-900 text-3xl md:text-2xl sm:text-2xl font-semibold font-stone-400">
+            Why would you hire me?
+          </h1>
+          {skills.map((item) => (
             <div
-              className={`text-lg font-semibold p-2 xl:p-1 border-dashed flex justify-between lg:text-md md:text-md sm:w-full ${
-                data.mode
-                  ? "text-violet-800 border-2 border-violet-600"
-                  : "text-yellow-500 border-2 border-amber-500"
-              }`}
+              key={item.id}
+              className={`my-4 lg:my-2.5 md:my-2.5 text-lg sm:text-sm text-justify  flex flex-col`}
             >
-              {item.title}
-              <SignalCellularAltIcon className="my-auto ml-[8%]" />
+              <div
+                className={`text-lg font-semibold p-2 xl:p-1 border-dashed flex justify-between lg:text-md md:text-md sm:w-full ${
+                  data.mode
+                    ? "text-violet-800 border-2 border-violet-600"
+                    : "text-yellow-500 border-2 border-amber-500"
+                }`}
+              >
+                {item.title}
+                <SignalCellularAltIcon className="my-auto ml-[8%]" />
+              </div>
             </div>
+          ))}
+        </div>
+        {/* Programming skills */}
+        <div className="content w-full p-5 rounded-lg border-2 border-indigo-900">
+          <h1 className="text-2xl font-semibold text-indigo-00">Skills</h1>
+          <div
+            className={`w-full sm:w-[calc(100%-10px)] sm:mx-auto rounded-md  ${
+              data.mode ? "bg-indigo-100" : "bg-[#313552]"
+            } ${data.mode ? "text-slate-800" : "text-black"}`}
+          >
+            {bars.map((bar) => (
+              <div key={bar.tag} className="my-2">
+                <div className="flex my-1 justify-between">
+                  <span className="font-semibold">{bar.tag}</span>
+                  <span className="font-semibold">{bar.value}%</span>
+                </div>
+                <div className="w-full bg-white h-3 rounded-lg overflow-hidden">
+                  <div
+                    className="h-full"
+                    style={{
+                      width: `${bar.value}%`,
+                      backgroundColor: bar.color,
+                      transition: "width 1s ease",
+                    }}
+                  ></div>
+                </div>
+              </div>
+            ))}
           </div>
-        ))}
+        </div>
       </div>
-      <div
+      {/* Programming skills */}
+      {/* <div
         className={`w-full sm:w-[calc(100%-10px)] sm:mx-auto rounded-md row-span-1 lg:row-span-1 col-span-2 sm:row-span-1 sm:col-span-2  grid grid-cols-4 ${
           data.mode ? "bg-indigo-100" : "bg-[#313552]"
         } ${data.mode ? "text-slate-800" : "text-black"}  `}
@@ -259,7 +315,7 @@ const Section2 = (props) => {
               data.mode ? "bg-indigo-200" : "bg-[#1f2235]"
             }`}
           >
-            <div className="mx-auto flex items-center justify-center w-[90px] xl:w-[65px] xl:h-[65px] h-[90px] lg:w-[80px] sm:w-[60px] lg:h-[80px] md:w-[80px] md:h-[80px] sm:w-[60px] sm:h-[60px]">
+            <div className="mx-auto flex items-center justify-center w-[90px] xl:w-[65px] xl:h-[65px] h-[90px] lg:w-[80px] lg:h-[80px] md:w-[80px] md:h-[80px] sm:w-[60px] sm:h-[60px]">
               <svg>
                 <circle
                   cx="50%"
@@ -293,7 +349,7 @@ const Section2 = (props) => {
             </div>
           </div>
         ))}
-      </div>
+      </div> */}
     </div>
   );
 };
